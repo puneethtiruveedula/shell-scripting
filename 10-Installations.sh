70c2de61ae7d82be7257b8fac0432aee3a9300da
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 12:22:17 +0530

    first commit

---
+++ 10-Installations.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access."
+else 
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+
+# echo $? --> exit status of previous command
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql...FAILURE"
+    exit1
+else 
+    echo "Insallation of mysql...SUCCESS"
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of Git...FAILURE"
+    exit1
+else 
+    echo "Insallation of Git...SUCCESS"
+
+
+echo "Is script proceeding?"
