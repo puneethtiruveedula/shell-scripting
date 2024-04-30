623d58f5ba19ca7c7b983133a2312fd91732ef01
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 12:08:31 +0530

    first commit

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bib/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+then
+    echo "Given number $NUMBER is greater than 10"
+else
+    echo "Given number $NUMBER is less than 10"
+fi
+
+
+# -gt , -lt, -ge, -le, -ne, -eq
