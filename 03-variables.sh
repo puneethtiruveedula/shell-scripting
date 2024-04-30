45234a4a75da9e85b88e360eaa0f86786d21de5d
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 09:07:37 +0530

    first commit

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring  of variables
+PERSON1=Ramesh
+PERSON2=Suresh
+
+#reflecting of variables
+echo "$PERSON1: Hi $PERSON2, How are you doing?"
+echo "Suresh: Hello $PERSPN1, I'm doing great. How are you?"
+echo "$PERSON1: I'm fine too, How is your work going?"
+echo "$PERSON2: Yeah it's going well, I'm thinkimg to learn DevOps"
