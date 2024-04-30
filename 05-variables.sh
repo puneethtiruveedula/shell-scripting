d8a98deb9acfbc7c5ffa5f31a28c13a70789a378
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 10:07:17 +0530

    first commit

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read USERNAME
+
+echo "Please enter password::"
+read PASSWORD
+
+echo "Username is:$USERNAME, Password is:$PASSWORD"
+
+# USE -s in the read --> invisible password and username
+
+
