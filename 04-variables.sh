7e787f620f80edacc0728abfb203254c9165b8fb
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 10:01:56 +0530

    first commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -9,3 +9,5 @@ echo "$PERSON1: Hi $PERSON2, How are you doing?"
 echo "$PERSON2: Hello $PERSPN1, I'm doing great. How are you?"
 echo "$PERSON1: I'm fine too, How is your work going?"
 echo "$PERSON2: Yeah it's going well, I'm thinkimg to learn DevOps"
+
+# sh 04-variables.sh puneeth suresh --> this replaces the names 
