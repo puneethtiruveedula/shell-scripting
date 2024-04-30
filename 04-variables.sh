9367308e999762e8e57b7aba800b22f17020e2de
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 09:59:13 +0530

    first commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,6 @@ PERSON2=$2
 
 #reflecting of variables
 echo "$PERSON1: Hi $PERSON2, How are you doing?"
-echo "Suresh: Hello $PERSPN1, I'm doing great. How are you?"
+echo "$PERSON2: Hello $PERSPN1, I'm doing great. How are you?"
 echo "$PERSON1: I'm fine too, How is your work going?"
 echo "$PERSON2: Yeah it's going well, I'm thinkimg to learn DevOps"
