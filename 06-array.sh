ed868fb486074137489ace8eaf3cf761028fa6c9
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 11:43:18 +0530

    first commit

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Movies=("RRR" "DJtillu" "oppenhiemer")
+
+#size of above array is 3
+#index are 0,1,2
+#list always start with 0
+
+echo "First moive: $1"
+echo "Second movie: $2"
+echo "Third movie: $3"
+echo "All movies: $@"
