1c244114511c84170cc3590c23a1b76d09d2701a
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 09:16:19 +0530

    first commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,4 +3,4 @@
 echo "Ramesh: Hi Suresh, How are you doing?"
 echo "Suresh: Hello Ramesh, I'm doing great. How are you?"
 echo "Ramesh: I'm fine too, How is your work going?"
-echo "Suresh: Yeah it's going well, I'm thinkimg to learn DevOps
+echo "Suresh: Yeah it's going well, I'm thinkimg to learn DevOps"
