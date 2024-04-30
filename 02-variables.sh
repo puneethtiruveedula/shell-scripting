b28cd41578c942b9de137c54b50b3b65d8b5c330
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-04-30 09:03:42 +0530

    first commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is again a sample script
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you doing?"
+echo "Suresh: Hello Ramesh, I'm doing great. How are you?"
+echo "Ramesh: I'm fine too, How is your work going?"
+echo "Suresh: Yeah it's going well, I'm thinkimg to learn DevOps
